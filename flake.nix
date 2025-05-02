2dccfd8a6f61989a00887ee1e5c89226394c7f79
David Shepard <daveman1010220@gmail.com>
2025-05-02 15:26:23 -0400

    flake update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -17,11 +17,11 @@
           src = pkgs.fetchFromGitHub {
             owner = "daveman1010221";
             repo = "dotacat-fast";
-            rev = "";
-            hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            rev = "8e0cb04b55e61f7a6b0599e589ae9456c9aa0fa6";
+            hash = "sha256-6yKr7lAwfvaq5IwmNUVewS+0hp27t0M5sokF6BaiLKg=";
           };
 
-          cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          cargoHash = "sha256-zA5VnMj+iyRas9dYVb5NwlPgMUNKSRwnlpK3zp5i+ls=";
 
           useFetchCargoVendor = true;
 
