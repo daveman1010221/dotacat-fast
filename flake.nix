1eeed96522cf18a1e303aeeeb597e8b943a78b9d
David Shepard <daveman1010220@gmail.com>
2025-05-02 11:50:33 -0400

    Fix the flake

--- flake.nix
+++ flake.nix
@@ -18,10 +18,11 @@
             owner = "daveman1010221";
             repo = "dotacat-fast";
             rev = "b35f15384dd2bbb2e6f9d4638ce9ced7a3336531";
-            hash = pkgs.lib.fakeSha256;
+            hash = "sha256-NPbgp94Dz1BZLI8DEUwSeHMzGV9RprCdWAtHsq5eaLs="; # dummy hash
           };
 
-          cargoHash = pkgs.lib.fakeSha256;
+          cargoHash = "sha256-lX84r7hH5hFPF76ZG/9TSMDAHSSz30B99iOT7sDBXPA="; # dummy hash
+
           useFetchCargoVendor = true;
 
           meta = with pkgs.lib; {
